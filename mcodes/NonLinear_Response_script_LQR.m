9603aea840107c1682f0fe6eedd2b66d0fe1740b
Srujan Panuganti <panuganti.srujan@gmail.com>
2020-05-12 00:32:01 -0400

    Add files via upload

---
+++ mcodes/NonLinear_Response_script_LQR.m
@@ -0,0 +1,63 @@
+M=1000;
+m1=100;
+m2=100;
+l1=20;
+l2=10;
+g=9.81;
+I = [1;1;5;0;5;0];
+%{
+Try the following sets of values values 
+---> Initial COndition: I = [0;0;0;0;0;0]
+---> I = [0;0;5;0;0;0]; t = 1:0.001:10;
+---> I = [1;0;5;0;5;0]; t = 1:0.0001:10;
+---> I = [1;0;5;0;5;0]; t = 1:0.001:10;
+---> I = [0;0;5;0;10;0]; t = 1:0.001:10;
+I = [1;1;5;0;5;0]; t = 1:0.0001:10;
+%}
+t = 1:0.0001:10;
+[t,x] = ode45(@nlsys_solve,t,I);
+plot(t,x,'linewidth',1);
+title('response of the non-liear system');
+
+%%Function Definition
+function nlsys= nlsys_solve(t,x)
+M=1000;
+m1=100;
+m2=100;
+l1=20;
+l2=10;
+g=9.81;
+
+A_f =[0 1 0 0 0 0;0 0 (-m1*g)/M 0 (-m2*g)/M 0;0 0 0 1 0 0;0 0 -(g/l1)*((m1+M)/M) 0 (-m2*g)/M*l1 0;0 0 0 0 0 1;0 0 (-m1*g)/M*l2 0 -(g/l2)*((m2+M)/M) 0];
+B_f =[0;1/M;0;1/(M*l1);0;1/(M*l2)];
+eigs(A_f);
+C = [1,0,0,0,0,0];
+%C = diag([1 1 1 1 1 1]);
+%C1 = [1,0,0,0,0,0];
+%D = [0;0;0;0;0;0];
+D = [0];
+
+Q = [1200000000 0 0 0 0 0;0 2 0 0 0 0;0 0 11000000000 0 0 0 ; 0 0 0 0.1 0 0;0 0 0 0 14000000000 0;0 0 0 0 0 1];
+R = 10;
+eig_Q = eigs(Q);
+N = [0;0;0;0;0;0];
+
+[K2,S2,e2] = lqr(A_f+eye(size(A_f)),B_f,Q,R,N);
+
+F = -K2*x;
+
+nlsys = zeros(6,1);
+
+nlsys(1)= x(2);
+
+nlsys(2)= ((F) - m1*g*sind(x(3))*cosd(x(3)) - m1*l1*x(4)^2*sind(x(3)) - m2*l2*x(6)^2*sind(x(5))  - m2*g*sind(x(5))*cosd(x(5)))/(M+m1*(sind(x(3)))^2+ m2*(sind(x(5)))^2);
+
+nlsys(3)= x(4);
+
+nlsys(4)= (((F) - m1*g*sind(x(3))*cosd(x(3)) - m1*l1*x(4)^2*sind(x(3)) - m2*l2*x(6)^2*sind(x(5))  - m2*g*sind(x(5))*cosd(x(5)))*cosd(x(3))/(M+m1*(sind(x(3)))^2+ m2*(sind(x(5)))^2)*l1)-((g*sind(x(3)))/l1);
+
+nlsys(5)= x(6);
+
+nlsys(6)= (((F) - m1*g*sind(x(3))*cosd(x(3)) - m1*l1*x(4)^2*sind(x(3)) - m2*l2*x(6)^2*sind(x(5))  - m2*g*sind(x(5))*cosd(x(5)))*cosd(x(5))/(M+m1*(sind(x(3)))^2+ m2*(sind(x(5)))^2)*l2)-((g*sind(x(5)))/l1);
+
+end
